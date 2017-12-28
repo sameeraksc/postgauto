5bd940accb95ea6ebe5850ea1f514879d6060727
sameeraksc <sameeraksc@gmail.com>
2017-12-28 01:03:02 -0500

    last comit

--- post.sh
+++ post.sh
@@ -1,58 +1,58 @@
 #!/bin/bash
 
-#DIRECTORY='/usr/local/pgsql/data'
-#
-#
-#
-#if [ $USER == "root" ];then
-#    
-#	cat /etc/passwd | grep postgres > /dev/null 2>&1
-#	if [ $? -ne 0 ];then
-#	   useradd postgres
-#           echo "New postgres User Created"
-#	fi
-#
-#   yum -y install zlib*
-#   yum -y install readline*
-#   yum -y install gcc*
-#   yum -y install tcl-devel
-#   yum -y install make
-#   
-#   if [ ! -d "/usr/local/pgsql/data" ];then
-#      mkdir /usr/local/pgsql/data -p
-#      chown postgres.postgres /usr/local/pgsql/data
-#      echo "path ownership created"
-#         
-#   fi
-#
-#wget -P /usr/local/pgsql $1
-#balla=$(ls /usr/local/pgsql | grep post)
-#echo "Untar $balla file.It takes some time"
-#cd /usr/local/pgsql
-#tar --checkpoint=.100  -xzf "$balla"
-#echo " "
-#patheka=$(find . -type d -iname "post*"| head -1 )
-#echo "$patheka"
-#cd "$patheka" && mv * ..
-#cd .. && ./configure
-#gmake install 
-#
-#   if [ ! -d "/rezsystem/rezadmin" ];then
-#      mkdir /rezsystem/rezadmin  -p
-#      echo "path created"
-#   fi
-#      
+DIRECTORY='/usr/local/pgsql/data'
+
+
+
+if [ $USER == "root" ];then
+    
+	cat /etc/passwd | grep postgres > /dev/null 2>&1
+	if [ $? -ne 0 ];then
+	   useradd postgres
+           echo "New postgres User Created"
+	fi
+
+   yum -y install zlib*
+   yum -y install readline*
+   yum -y install gcc*
+   yum -y install tcl-devel
+   yum -y install make
+   
+   if [ ! -d "/usr/local/pgsql/data" ];then
+      mkdir /usr/local/pgsql/data -p
+      chown postgres.postgres /usr/local/pgsql/data
+      echo "path ownership created"
+         
+   fi
+
+wget -P /usr/local/pgsql $1
+balla=$(ls /usr/local/pgsql | grep post)
+echo "Untar $balla file.It takes some time"
+cd /usr/local/pgsql
+tar --checkpoint=.100  -xzf "$balla"
+echo " "
+patheka=$(find . -type d -iname "post*"| head -1 )
+echo "$patheka"
+cd "$patheka" && mv * ..
+cd .. && ./configure
+gmake install 
+
+   if [ ! -d "/rezsystem/rezadmin" ];then
+      mkdir /rezsystem/rezadmin  -p
+      echo "path created"
+   fi
+      
 #sed -i '2s/$/ export PATH=$PATH:\/rezsystem\/rezadmin/' /etc/bashrc
 #bash
-#cd /rezsystem/rezadmin/ && touch pgstop pgstart
-#echo "pkill post*" > pgstop
-#chown postgres.postgres pgstop
-#chmod u+x pgstop
-#
-#echo "nohup /usr/local/pgsql/bin/postgres -D /usr/local/pgsql/data &" > pgstart
-#chown postgres.postgres pgstart
-#chmod u+x pgstart
-##########################################have to input details to pgstop start
+cd /rezsystem/rezadmin/ && touch pgstop pgstart
+echo "pkill post*" > pgstop
+chown postgres.postgres pgstop
+chmod 700 pgstop
+
+echo "nohup /usr/local/pgsql/bin/postgres -D /usr/local/pgsql/data &" > pgstart
+chown postgres.postgres pgstart
+chmod u+x pgstart
+#########################################have to input details to pgstop start
 
 
 su postgres << 'EOT'
@@ -61,10 +61,10 @@ su postgres << 'EOT'
 
 EOT
 
-#echo `whoami`
-#
-#else
-#
-#  echo "you must become root"
-#  exit 1 ;
-#fi
+echo `whoami`
+
+else
+
+  echo "you must become root"
+  exit 1 ;
+fi
